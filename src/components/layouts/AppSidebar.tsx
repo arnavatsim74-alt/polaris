5929926aa9c09ea50d646e29fff876b27320fc5a
arnavatsim74-alt <arnavatsim74@gmail.com>
2026-02-27 20:59:58 +0530

    Rebrand LATV/LATOUR Virtual text to KEVA/Koreanair Virtual

--- src/components/layouts/AppSidebar.tsx
+++ src/components/layouts/AppSidebar.tsx
@@ -104,7 +104,7 @@ export function AppSidebar() {
     <Sidebar collapsible="icon">
       <SidebarHeader className="border-b border-sidebar-border">
         <div className={cn("flex items-center gap-3 px-3 py-2", isCollapsed && "justify-center")}>
-          <img src={logoSrc} alt="LATOUR Virtual" className={cn("w-auto object-contain", isCollapsed ? "h-8" : "h-10")} />
+          <img src={logoSrc} alt="Koreanair Virtual" className={cn("w-auto object-contain", isCollapsed ? "h-8" : "h-10")} />
         </div>
       </SidebarHeader>
 
